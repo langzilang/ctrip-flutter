8b4f1fc17210c1975b3373eb37bb07902a3cd085
langzilang <1874815111@qq.com>
2021-05-27 23:04:15 +0800

    commit

---
+++ lib/tab_navigator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabNavigator extends StatefulWidget {
+  const TabNavigator({Key key}) : super(key: key);
+
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
