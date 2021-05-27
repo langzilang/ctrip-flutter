44a48b0b78f8b4df778764bf796d438482c7090a
langzilang <1874815111@qq.com>
2021-05-28 03:16:28 +0800

    content_pager first

---
+++ lib/content_pager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContentPager extends StatefulWidget {
+  const ContentPager({Key key}) : super(key: key);
+
+  @override
+  _ContentPagerState createState() => _ContentPagerState();
+}
+
+class _ContentPagerState extends State<ContentPager> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
