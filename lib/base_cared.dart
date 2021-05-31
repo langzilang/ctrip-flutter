be17ae0362d1ff164de0cb7b5b76e211ee58c053
langzilang <1874815111@qq.com>
2021-06-01 03:12:09 +0800

    base_cared

---
+++ lib/base_cared.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+//卡片基类
+class BaseCared extends StatefulWidget {
+  const BaseCared({Key key}) : super(key: key);
+
+  @override
+  BaseCaredState createState() => BaseCaredState();
+}
+
+class BaseCaredState extends State<BaseCared> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/card_recommend.dart
@@ -0,0 +1,11 @@
+//卡片基类
+import 'package:flutter_app/base_cared.dart';
+
+//本周推介
+class CardRecommend extends BaseCared {
+  @override
+  _CardRecommendState createState()  => _CardRecommendState();
+}
+class _CardRecommendState extends BaseCaredState{
+
+}
