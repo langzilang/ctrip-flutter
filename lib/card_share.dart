abd56f8e08b17f39cf3b5036f939b2c8a9290484
langzilang <1874815111@qq.com>
2021-06-02 04:17:00 +0800

    card_share first

---
+++ lib/card_share.dart
@@ -0,0 +1,11 @@
+//分享得联名卡
+import 'package:flutter_app/base_cared.dart';
+
+class CardShare extends BaseCared{
+
+  @override
+  _CardShareState createState() => _CardShareState();
+}
+class _CardShareState extends BaseCaredState{
+
+}
